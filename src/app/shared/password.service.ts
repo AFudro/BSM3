ecb0f2412245c42d668bde6759bc8e2f0e3b296c
Andrzej Fudro <andrzej.fudro@gmail.com>
2018-11-13 01:27:17 +0100

    Note encryption

--- src/app/shared/password.service.ts
+++ src/app/shared/password.service.ts
@@ -1,11 +1,12 @@
 import * as appSettings from 'tns-core-modules/application-settings/application-settings';
 import { Injectable } from '@angular/core';
 var CryptoJS = require("crypto-js");
+
 @Injectable()
 export class PasswordService {
 
     securedPassword: any;
-    aesKey: string; 
+    aesKey: string;
 
     constructor() {
         this.securedPassword = appSettings.getString('password') ? JSON.parse(appSettings.getString('password')) : undefined;
@@ -16,28 +17,36 @@ export class PasswordService {
     }
 
     async setPassword(password: String) {
-        this.securedPassword = await CryptoJS.PBKDF2(password, 'salt', 100, 64, 'sha512');
-        appSettings.setString('password', JSON.stringify(this.securedPassword));
+        const aesKey: string = this.getPbkdf2(password);
+        const ciphertext = CryptoJS.AES.encrypt(password, aesKey);
+        this.securedPassword = ciphertext;
+        appSettings.setString('password', JSON.stringify(ciphertext.toString()));
     }
 
     async comparePassword(password: String): Promise<boolean> {
-        const pbkdf2 = await CryptoJS.PBKDF2(password, 'salt', 100, 64, 'sha512');
-        // console.log(this.securedPassword);
-        // console.log(pbkdf2);
-        // console.log('----');
-        if (JSON.stringify(pbkdf2) === JSON.stringify(this.securedPassword)) {
-            this.aesKey = pbkdf2;
-            console.log(this.aesKey);
-            return true
-    }
+        const aesKey: string = this.getPbkdf2(password);
+        const bytes  = CryptoJS.AES.decrypt(this.securedPassword, aesKey);
+        const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+            if (password === decryptedPassword) {
+                this.aesKey = aesKey;
+                return true
+        }
         return false;
     }
 
-saveNote(note: String) {
-    appSettings.setString('note', JSON.stringify(note));
-}
+    saveNote(note: String) {
+        const securedNote = CryptoJS.AES.encrypt(note, this.aesKey);
+        appSettings.setString('note', JSON.stringify(securedNote.toString()));
+    }
 
-getNote() {
-    return appSettings.getString('note') ? JSON.parse(appSettings.getString('note')) : '';
-}
+    getNote() {
+        const note = appSettings.getString('note') ? JSON.parse(appSettings.getString('note')) : '';
+        const bytes  = CryptoJS.AES.decrypt(note, this.aesKey);
+        return bytes.toString(CryptoJS.enc.Utf8);
+    }
+
+    getPbkdf2(password: String) {
+        const pbkdf2 = CryptoJS.PBKDF2(password, 'salt', 100, 64, 'sha512');
+        return pbkdf2.toString();
+    }
 }
